bfbb8c5aaaf6b7820d9b05ea410e620bd9620b34
Dyu <apostoljustine123@gmail.com>
2026-03-27 21:31:11 +0800

    Bump app version to 2.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.dyu.ereader"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "2.2.3"
+        versionCode = 10
+        versionName = "2.2.4"
         buildConfigField("String", "MOBI_CONVERTER_URL", "\"\"")
         buildConfigField("String", "DROPBOX_APP_KEY", "\"${dropboxAppKey.get()}\"")
         buildConfigField("String", "GITHUB_RELEASE_OWNER", "\"${githubReleaseOwner.get()}\"")
